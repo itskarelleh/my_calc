bdc840d50cabb54cdfb200ce7c0dacb96494c9c9
khofler94 <karellehofler@gmail.com>
2017-11-18 18:52:30 -0500

    Calculator layout styled

---
+++ js/script.js
@@ -0,0 +1,24 @@
+(document).ready(function(){
+    //Stores Inputs
+    var inputs = [""];
+
+    //String to store current input
+    var totatString;
+
+    var operators1 = ["+", "-", "*", "/"];
+
+    var operators2 = ["."];
+    var nums = [0,1,2,3,4,5,6,7,8,9];
+
+    function getValue(input){
+
+    }
+
+    function update(){
+
+    }
+
+    function getTotal(){
+        
+    }
+});
